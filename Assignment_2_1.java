46cbabc45fc5d2a5b4cb5181f89c3bbbd037c316
Subhamsah713213 <subhamsah713213@gmail.com>
2016-12-21 17:08:27 +0530

    Add files via upload

---
+++ Assignment_2_1.java
@@ -0,0 +1,12 @@
+package com.Subham;
+
+public class Assignment_2_1 {
+	public static void main(String args[])    /* main method */
+	{
+		if(System.out.printf("Subham Kumar Sah") != null)     /* my name is SUBHAM KUMAR SAH*/
+		{
+			
+		}
+	}
+
+}
